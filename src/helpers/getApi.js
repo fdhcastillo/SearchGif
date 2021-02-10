d6d00e7aac543e6c30e28c996605d44171a20729
Back2You <francohmmm_29@hotmail.com>
2021-02-10 18:12:08 -0500

    index actualizado 2

--- src/helpers/getApi.js
+++ src/helpers/getApi.js
@@ -1,5 +1,5 @@
 export const getApi = async(category) => {
-  const url = `http://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=V6QGYoLheM381AuLKVUEnQTpe3CshAGw`;
+  const url = `https://api.giphy.com/v1/gifs/search?q=${encodeURI(category)}&limit=10&api_key=V6QGYoLheM381AuLKVUEnQTpe3CshAGw`;
   const resp = await fetch(url);
   const {data} = await resp.json();
   const gifs = data.map( image => {
